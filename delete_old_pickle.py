cd5773ff3cf7979e1f3531a333fb640cce868067
evgeniy <pavlinbl4@gmail.com>
2024-02-18 22:53:14 +0300

    error with filename solved

--- delete_old_pickle.py
+++ delete_old_pickle.py
@@ -1,14 +1,20 @@
 import glob
 import os
+from pathlib import Path
+import re
 
+def file_name(pickle_file):
+    return re.findall(r'\d{2}_\d{2}_\d{2}', pickle_file)[0]
 
 def delete_old_pickle(today_filename):
-    if len(glob.glob(f'*.pickle')) != 0:
-        pickle_file = glob.glob(f'*.pickle')[0]
-        print(pickle_file)
-        if pickle_file != today_filename:
+    if len(glob.glob(f'{Path.home()}/*.pickle')) != 0:
+        pickle_file = glob.glob(f'{Path.home()}/*.pickle')[0]
+        # print(file_name(pickle_file))
+        if file_name(pickle_file) != today_filename:
             os.remove(pickle_file)
+        # print("fresh file")
 
 
 if __name__ == '__main__':
-    delete_old_pickle("eee")
+    delete_old_pickle("18_02_24.pickle")
+    # print(file_name('/Users/evgeniy/18_02_24.pickle'))
