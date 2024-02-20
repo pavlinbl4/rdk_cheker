8ca54cd42f97c497b8709cca8f934e2830c952bd
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-02-20 22:05:33 +0300

    and print to control function

--- check_article_status.py
+++ check_article_status.py
@@ -1,18 +1,25 @@
 import pickle
 from send_message_to_telegram import send_telegram_message
 from pathlib import Path
+from icecream import ic
 
 
 def check_article_status(article_name, article_status, today_filename):
     with open(f'{Path.home()}/{today_filename}.pickle', 'rb') as articles:
         article_dict = pickle.load(articles)
+        # ic(article_dict)
 
     if article_dict[article_name] != article_status and article_status == '***&site':
         send_telegram_message(article_name)
+        print(f'old status of {article_name} - {article_status}')
         article_dict[article_name] = article_status
+        print(f'new status of {article_name} - {article_status}')
+
         with open(f'{Path.home()}/{today_filename}.pickle', 'wb') as f:
             pickle.dump(article_dict, f)
+    else:
+        print("No changes in pickle file")
 
 
 if __name__ == '__main__':
-    check_article_status('вайлдберис на утро среды', '&site', 'dddd')
+    check_article_status('фарватер на утро', '***&site', '20_02_24')
