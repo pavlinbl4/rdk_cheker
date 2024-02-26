4dd15410982ae60161f1aebeb871b3d393b053ab
evgeniy <pavlinbl4@gmail.com>
2024-02-26 15:00:39 +0300

    get city timezone

---
+++ time_zone.py
@@ -0,0 +1,17 @@
+import datetime
+import pytz
+
+
+# Function to return current time for a given city
+def get_city_time(city):
+    # Get timezone from dictionary
+    tz = pytz.timezone(city)
+    # Localize current datetime to city timezone
+    now = datetime.datetime.now(tz)
+    # Return the current time
+    return now.strftime("%H:%M:%S - %H:%M")
+
+
+if __name__ == '__main__':
+    print(get_city_time('Europe/London'))  # => 14:15:23
+    print(get_city_time('Europe/Moscow'))
