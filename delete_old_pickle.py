ae3ace9a54d971a8b0472b8432607a2c97adc2c5
evgeniy <pavlinbl4@gmail.com>
2024-02-26 17:26:47 +0300

    add 'pickle_folder' as argument

--- delete_old_pickle.py
+++ delete_old_pickle.py
@@ -4,11 +4,11 @@ from pathlib import Path
 import re
 
 def file_name(pickle_file):
-    return re.findall(r'\d{2}_\d{2}_\d{2}', pickle_file)[0]
+    return re.findall(r'\d{2}-\d{2}-\d{2}', pickle_file)[0]
 
-def delete_old_pickle(today_filename):
-    if len(glob.glob(f'{Path.home()}/*.pickle')) != 0:
-        pickle_file = glob.glob(f'{Path.home()}/*.pickle')[0]
+def delete_old_pickle(today_filename,pickle_folder):
+    if len(glob.glob(f'{pickle_folder}/*.pickle')) != 0:
+        pickle_file = glob.glob(f'{pickle_folder}/*.pickle')[0]
         # print(file_name(pickle_file))
         if file_name(pickle_file) != today_filename:
             os.remove(pickle_file)
@@ -16,5 +16,5 @@ def delete_old_pickle(today_filename):
 
 
 if __name__ == '__main__':
-    delete_old_pickle("18_02_24.pickle")
+    delete_old_pickle("28_02_24.pickle", 'Piclkle_files')
     # print(file_name('/Users/evgeniy/18_02_24.pickle'))
