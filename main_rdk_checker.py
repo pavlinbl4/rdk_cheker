7f79e5ce4c4ac91c0955d23e0c1a8e54da2e7515
evgeniy <pavlinbl4@gmail.com>
2024-02-27 20:02:46 +0300

    problem with folder and path to pickle file

--- main_rdk_checker.py
+++ main_rdk_checker.py
@@ -1,9 +1,9 @@
 # pip install python-dotenv
 # pip install webdriver-manager
 
-
+from icecream import ic
 import re
-import pickle
+
 
 from selenium import webdriver
 from datetime import date
@@ -16,6 +16,7 @@ from selenium.webdriver.firefox.service import Service
 
 from check_article_status import check_article_status
 from delete_old_pickle import delete_old_pickle
+from pickle_files import load_pickle, dump_pickle
 from send_message_to_telegram import send_telegram_message
 from selenium.webdriver import FirefoxOptions
 
@@ -58,17 +59,17 @@ def get_today_link(all_spans):
 
 
 def get_article_status():
-    pickle_folder = 'Piclkle_files'
+    pickle_folder = 'Pickle_files'
     today_filename = get_city_time('Europe/Moscow').strftime("%d-%m-%Y")
     delete_old_pickle(today_filename, pickle_folder)
     all_spans = get_spans()
     article_dict = {}
 
-    (Path() / pickle_folder).mkdir(exist_ok=True)
+    (Path() / pickle_folder).mkdir(exist_ok=True)  # create folder for pickle files
 
     if os.path.exists(f'{pickle_folder}/{today_filename}.pickle'):
-        with open(f'{pickle_folder}/{today_filename}.pickle', 'rb') as data_file:
-            article_dict = pickle.load(data_file)
+        print('Pickle file exist')
+        article_dict = load_pickle(pickle_folder, today_filename)
     today_link = get_today_link(all_spans)
     driver.get(today_link)  # '//tr[@class="mapLO"][2]/td[6]/img'
     work_map = driver.find_elements('xpath', '//tr[@class="mapLO"]')
@@ -78,12 +79,14 @@ def get_article_status():
         article_name = all_trs[0].text
         article_status = find_article_status(all_trs[5].find_element('xpath', 'img').get_attribute('src'))
         if len(article_name) > 0 and article_name not in article_dict:
-            print(f'{article_name} -- {article_status}')
+            print(f'added to dict : {article_name} -- {article_status}')
 
-            send_telegram_message(f'{article_name} - {article_status}')
+            # send_telegram_message(f'{article_name} - {article_status}')
             article_dict[article_name] = article_status
-            with open(f'{pickle_folder}/{today_filename}.pickle', 'wb') as f:
-                pickle.dump(article_dict, f)
+            # with open(f'{pickle_folder}/{today_filename}.pickle', 'wb') as f:
+            #     pickle.dump(article_dict, f)
+            dump_pickle(pickle_folder, today_filename, article_dict)
+            print("Pickle file updated")
         else:
             check_article_status(article_name, article_status, today_filename, pickle_folder)
 
