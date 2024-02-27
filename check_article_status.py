a4a975344c4ddc2502dba062b9c3d439bfca0ee8
evgeniy <pavlinbl4@gmail.com>
2024-02-27 17:34:31 +0300

    create separate function to pickle files^ add try-except

--- check_article_status.py
+++ check_article_status.py
@@ -1,30 +1,29 @@
-import pickle
+from pickle_files import load_pickle, dump_pickle
 from send_message_to_telegram import send_telegram_message
-from pathlib import Path
-# from icecream import ic
 import logging
 
-logging.basicConfig(level=logging.INFO, filename='my_lol.log', datefmt='$Y/$m/$d  %I/%M', encoding='utf-8', filemode='w+')
+logging.basicConfig(level=logging.INFO, filename='my_lol.log', datefmt='$Y/$m/$d  %I/%M', encoding='utf-8',
+                    filemode='w+')
 
 
+def check_article_status(article_name, article_status, today_filename, pickle_folder):
+    article_dict = load_pickle(pickle_folder, today_filename)
 
-def check_article_status(article_name, article_status, today_filename,pickle_folder):
-    with open(f'{pickle_folder}/{today_filename}.pickle', 'rb') as articles:
-        article_dict = pickle.load(articles)
-        # ic(article_dict)
+    try:
+        if article_dict[article_name] != article_status and article_status == '***&site':
+            send_telegram_message(article_name)
+            logging.info(f'old status of {article_name} - {article_status}')
+            print(f'old status of {article_name} - {article_status}')
+            article_dict[article_name] = article_status
+            print(f'new status of {article_name} - {article_status}')
 
-    if article_dict[article_name] != article_status and article_status == '***&site':
-        send_telegram_message(article_name)
-        logging.info(f'old status of {article_name} - {article_status}')
-        print(f'old status of {article_name} - {article_status}')
-        article_dict[article_name] = article_status
-        print(f'new status of {article_name} - {article_status}')
-
-        with open(f'{pickle_folder}/{today_filename}.pickle', 'wb') as f:
-            pickle.dump(article_dict, f)
-    else:
-        print("No changes in pickle file")
+            dump_pickle(pickle_folder, today_filename, article_dict)
+        else:
+            print("No changes in pickle file")
+    except KeyError as key:
+        send_telegram_message(f'No such line in RDK - {key}')
+        print(f'No such line in RDK - {key}')
 
 
 if __name__ == '__main__':
-    check_article_status('фарватер на утро', '***&site', '20_02_24')
+    check_article_status('фарватер на утро', '***&site', '26-02-2024', 'Pickle_files')
