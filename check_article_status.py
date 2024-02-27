bbc6d0c000599636c7f9198b9908223ed0a1425c
evgeniy <pavlinbl4@gmail.com>
2024-02-27 20:03:05 +0300

    problem with folder and path to pickle file

--- check_article_status.py
+++ check_article_status.py
@@ -1,6 +1,7 @@
 from pickle_files import load_pickle, dump_pickle
 from send_message_to_telegram import send_telegram_message
 import logging
+from icecream import ic
 
 logging.basicConfig(level=logging.INFO, filename='my_lol.log', datefmt='$Y/$m/$d  %I/%M', encoding='utf-8',
                     filemode='w+')
@@ -10,12 +11,17 @@ def check_article_status(article_name, article_status, today_filename, pickle_fo
     article_dict = load_pickle(pickle_folder, today_filename)
 
     try:
-        if article_dict[article_name] != article_status and article_status == '***&site':
+        ic(article_dict[article_name], article_status)
+        # ic(article_dict[article_name] != article_status and article_status == '***&site')
+        # ic(article_status == '***&site')
+        # ic(article_dict[article_name] != article_status)
+        # if article_dict[article_name] != article_status and article_status == '***&site':
+        if article_status == '***&site':
             send_telegram_message(article_name)
             logging.info(f'old status of {article_name} - {article_status}')
-            print(f'old status of {article_name} - {article_status}')
+            # print(f'old status of {article_name} - {article_status}')
             article_dict[article_name] = article_status
-            print(f'new status of {article_name} - {article_status}')
+            # print(f'new status of {article_name} - {article_status}')
 
             dump_pickle(pickle_folder, today_filename, article_dict)
         else:
@@ -26,4 +32,5 @@ def check_article_status(article_name, article_status, today_filename, pickle_fo
 
 
 if __name__ == '__main__':
-    check_article_status('фарватер на утро', '***&site', '26-02-2024', 'Pickle_files')
+    # check_article_status('фарватер на утро', '***&site', '26-02-2024', 'Pickle_files')
+    check_article_status('Юлию Харитону 120 лет', '***&site', '27-02-2024', 'Pickle_files')
