0549762b2812f2f2951adb9faa23820e53fad6de
evgeniy <pavlinbl4@gmail.com>
2024-03-06 14:07:10 +0300

    removed needless code

--- main_rdk_checker.py
+++ main_rdk_checker.py
@@ -1,56 +1,42 @@
-
 from check_article_status import check_article_status
 from check_existing_file import create_dir
 from driver_job import get_work_map
 from pickle_files import load_pickle, dump_pickle, delete_old_pickle
 from time_zone import get_city_time
 from loguru import logger
+import os
 
 logs_dir = create_dir('Logs')
 # logs_dir = 'Logs'
 # custom_formatter = "<red>{time:YYYY-MM-DD HH:mm}</red> | <level>{level}</level> | <cyan>{message}</cyan>"
 # logger.add(f'{logs_dir}/debug.log', format=custom_formatter)
-logger.add(f'{logs_dir}/debug.log')
+logger.add(f'{logs_dir}/debug.log', rotation="10:00")
 
 
 def get_article_status():
     # create today day string look like '28-02-2024'
     today_filename = get_city_time('Europe/Moscow').strftime("%d-%m-%Y")
-
-    # create dict for articles
-    article_dict = {}
+    # today_filename = 'RDK'
+    logger.info(today_filename)
 
     # set folder to pickle files
     pickle_folder = 'Pickle_files'
-    path_to_pickle_folder = create_dir(pickle_folder)  # create folder for pickle files
+    path_to_pickle_folder = create_dir(pickle_folder)
     # logger.info(f"{path_to_pickle_folder}")
 
     # delete old pickle file
     delete_old_pickle(today_filename, path_to_pickle_folder)
 
     # create new pickle file
-    dump_pickle(path_to_pickle_folder, today_filename, article_dict)
+    dump_pickle(path_to_pickle_folder, today_filename)
 
-    # # if picle file exist load info from pickle to article_dict
-    # if os.path.exists(f'{path_to_pickle_folder}/{today_filename}.pickle'):
-    #     logger.info(f"file {today_filename}.pickle exist")
-    #     article_dict = load_pickle(path_to_pickle_folder, today_filename)
-    # else:
-    #     logger.info(f"No {today_filename}.pickle now")
+    article_dict = load_pickle(path_to_pickle_folder, today_filename)
 
     # get information from RDK site
-    fresh_article_dict = get_work_map()
+    article_dict = get_work_map(article_dict)
+    logger.info(article_dict)
 
-    if article_dict != fresh_article_dict:
-        article_dict.update(fresh_article_dict)
-
-        dump_pickle(path_to_pickle_folder, today_filename, article_dict)
-
-        logger.info("Pickle file updated")
-    else:
-        logger.info(f"No new articles in RDK now")
-
-    check_article_status(today_filename, path_to_pickle_folder)
+    check_article_status(today_filename, path_to_pickle_folder, article_dict)
 
 
 if __name__ == '__main__':
