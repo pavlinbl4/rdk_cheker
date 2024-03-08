38b95197aa6a8c50bda78147eb342c5ce0c15f28
evgeniy <pavlinbl4@gmail.com>
2024-03-08 21:27:00 +0300

    __file__ is the pathname of the file from which the module was loaded, if it was loaded from a file

---
+++ check_existing_file.py
@@ -0,0 +1,20 @@
+import os
+
+
+def check_file(file_name: str) -> object:
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, file_name)
+    return os.path.isfile(file_path)
+
+
+def create_dir(folder_name: str):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    folder_path = os.path.join(script_dir, folder_name)
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+    return folder_path
+
+
+if __name__ == '__main__':
+    assert check_file('requirements.txt') is True
+    assert check_file('27-02-2024.pickle') is False
