7bada4a04e9511f46f33250634a2459da70c63b6
evgeniy <pavlinbl4@gmail.com>
2024-03-09 20:43:09 +0300

    replaced

--- browser/driver_job.py
+++ browser/driver_job.py
@@ -1,6 +1,6 @@
 # pip install python-dotenv
 # pip install webdriver-manager
-import sys
+
 
 from dotenv import load_dotenv
 import os
@@ -12,13 +12,23 @@ from time_zone import get_city_time
 import re
 from loguru import logger
 
-# custom_formatter = "<red>{time:YYYY-MM-DD HH:mm}</red> | <level>{level}</level> | <cyan>{message}</cyan>"
-# logger.add(sink="stdout",format=custom_formatter)
+logger.disable("get_today_link")
 
 service = Service(GeckoDriverManager().install())
 options = FirefoxOptions()
 options.add_argument("--headless")
 driver = webdriver.Firefox(service=service, options=options)
+
+
+def find_article_status(article_status='GetImage.axd?kind=WF&key=E&site=RDK2SPB'):
+    pattern = r'(?<=key=).*(?==RDK2SPB)'
+    return re.findall(pattern, article_status)[0]
+
+
+def save_html_page(page_name):
+    page_source = driver.page_source
+    with open(f"{page_name}.html", "w", encoding="utf-8") as f:
+        f.write(page_source)
 
 
 def find_date(str_with_date='№24 Пн, 25.02.24'):
@@ -28,9 +38,12 @@ def find_date(str_with_date='№24 Пн, 25.02.24'):
 
 def get_today_link(all_spans):
     today = get_city_time('Europe/Moscow').strftime("%d.%m.%y")
+    # today = '04.03.24'
+    logger.info(today)
     for span in all_spans:
         if find_date(span.text) == today:
             today_link = span.get_attribute('href')
+            logger.info(f"{today_link =  } ")
             return today_link
 
 
@@ -39,19 +52,37 @@ def get_spans():
     rdk_logging = os.environ.get('rdk_logging')
     driver.get(rdk_logging)
     driver.get('https://rdk.spb.kommersant.ru:9443/rdk2/?p=RDK2SPB,NODE:2353975')
+    # driver.save_screenshot('rdk_page.png')
+
+    # save page as html
+    # save_html_page(driver, 'rdk_page.html')
+
     all_spans = driver.find_elements('xpath', '//span[@id="phList"]/a')
-    logger.info(f"{len(all_spans)}")
+    # logger.info(f"{len(all_spans)}")
     return all_spans
 
 
-def get_work_map():
+def get_work_map(article_dict: dict):
     all_spans = get_spans()
     today_link = get_today_link(all_spans)
     driver.get(today_link)  # '//tr[@class="mapLO"][2]/td[6]/img'
+
+    # save page as html
+    # save_html_page(driver, 'rdk_today')
+
     work_map = driver.find_elements('xpath', '//tr[@class="mapLO"]')
+
+    for x in range(1, len(work_map)):
+        all_trs = work_map[x].find_elements('xpath', 'td')
+        article_name = all_trs[0].text
+        article_status = find_article_status(all_trs[5].find_element('xpath', 'img').get_attribute('src'))
+        article_dict[article_name] = article_status
+        logger.info(article_name)
+
     driver.quit()
-    return work_map
+
+    return article_dict
 
 
 if __name__ == '__main__':
-    get_work_map()
+    get_work_map({'Юбилей первого выступления Утесова СТАВИМ ЗАВТРА': 'V&site'})
